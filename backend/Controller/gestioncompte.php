380b5e8bc484e5db61dbd6ef513016f2788cc4ac
Honhu Tiayon <ctiayon@gmail.com>
2026-04-28 11:26:59 +0100

    ajout CORS pour le frontend

--- backend/Controller/gestioncompte.php
+++ backend/Controller/gestioncompte.php
@@ -1,4 +1,13 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
