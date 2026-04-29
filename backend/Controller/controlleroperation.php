7b37b4c34a962d4a7db4a0cc25ac2f191204fa7a
nteupdieu <carlinvaldes119@gmail.com>
2026-04-29 09:50:57 +0100

    Update controlleroperation.php

--- backend/Controller/controlleroperation.php
+++ backend/Controller/controlleroperation.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization ");
+header("Content-Type: Application/json; charset=UTF-8");
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
